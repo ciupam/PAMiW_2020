82491d7efc573622be901601a948b32842aeb43f
MateuszCiupa <48243124+MateuszCiupa@users.noreply.github.com>
2019-12-07 16:50:00 +0100

    Codesandbox test

--- src/client/src/Components/Layouts/Home.js
+++ src/client/src/Components/Layouts/Home.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Paper, Typography } from '@material-ui/core';
-import useStyles from './Assets/useStyles';
+import React from "react";
+import { Paper, Typography } from "@material-ui/core";
+import useStyles from "./Assets/useStyles";
 
 export default props => {
     const classes = useStyles();
@@ -8,7 +8,7 @@ export default props => {
     return (
         <Paper className={classes.paper} square>
             <Typography variant="h2" align="center">
-                Yet another home page
+                Yet another home page page
             </Typography>
         </Paper>
     );
